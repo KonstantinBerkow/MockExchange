f9666c82cbeef92f3c666eb78eb0e6ea46eb4d25
kb <berkow@gmail.com>
2024-04-16 17:19:57 +0300

    add minimal balance fragment

---
+++ app/src/main/java/io/github/konstantinberkow/mockexchange/balance/BalanceFragment.kt
@@ -0,0 +1,9 @@
+package io.github.konstantinberkow.mockexchange.balance
+
+import androidx.fragment.app.Fragment
+import io.github.konstantinberkow.mockexchange.R
+
+class BalanceFragment : Fragment(R.layout.fragment_balance) {
+
+
+}
