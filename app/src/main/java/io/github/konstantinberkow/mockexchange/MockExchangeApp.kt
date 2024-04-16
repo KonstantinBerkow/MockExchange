d46fbebab0f8cb11f334959e1788c370a67c93c1
kb <berkow@gmail.com>
2024-04-17 02:38:18 +0300

    Start Koin in app

--- app/src/main/java/io/github/konstantinberkow/mockexchange/MockExchangeApp.kt
+++ app/src/main/java/io/github/konstantinberkow/mockexchange/MockExchangeApp.kt
@@ -1,4 +1,23 @@
 package io.github.konstantinberkow.mockexchange
 
-class MockExchangeApp {
+import android.app.Application
+import io.github.konstantinberkow.mockexchange.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MockExchangeApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            if (BuildConfig.DEBUG) {
+                androidLogger(level = Level.DEBUG)
+            }
+            androidContext(this@MockExchangeApp)
+            modules(appModule)
+        }
+    }
 }
